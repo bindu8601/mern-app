42153fd10d3a091bdf758bdf3b7c856364327256
bindu8601 <bindueppalapalle@gmail.com>
2024-07-23 11:28:41 +0530

    creating server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose=require('mongoose')
+const User=require('./models/User')
+const jwt=require('jsonwebtoken')
+
+const app = express();
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+console.log(process.env.MONGO_URL);
+const jwtSecret=process.env.JWT_SECRET
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', async(req, res) => {
+const {userName,passowrd}=req.body
+const createdUser= await User.create({userName,passowrd})
+jwt.sign({userId:createdUser,id},jwtSecret,(err,token)=>{
+    if(err) throw err;
+    res.cookie('token',token).status(201).json('ok');
+})
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(4000)
